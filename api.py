ba58e4b1a80213cb8fcc6a5b01a82db7725bc98a
grithaq <afifnurazizi@gmail.com>
2023-01-12 23:37:26 +0800

    docs: add name to attendes
    collecting audience name to attendes variable, when the audience register to the webinar

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 # Instance fast api object
 app = FastAPI()
 
+#attendes
+attendes = []
+
 @app.get("/")
 async def welcome_to_the_webinar() -> dict:
     return {
@@ -11,6 +14,7 @@ async def welcome_to_the_webinar() -> dict:
 
 @app.post("/attend")
 async def register_for_the_webinar(name: str):
+    attendes.append(name)
     return {
         "message" : f"Dear {name}, you have successfully registeredfor the webinar "
     }
